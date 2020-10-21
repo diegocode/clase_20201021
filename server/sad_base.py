0a04a4f4820291f03b83ebea4bc6772823631c8b
diegocode <diegocode@gmail.com>
2020-10-21 19:32:24 -0300

    se agregaron comentarios al código

--- server/sad_base.py
+++ server/sad_base.py
@@ -6,33 +6,53 @@ import datetime
 
 @route('/datain')
 def guardar_datos():
+    """
+        recibe y guarda los datos del request de clientes
+    """
     error_datain = 0
 
+    # obtiene temperatura, humedad, sensor del request 
     temp = request.query.tempe
     hume = request.query.hume
 
     sens_nro = request.query.snum
+
+    # si no hay sensor, le asigna 0
     if sens_nro == "":
         sens_nro = "0"
 
+    # fecha y hora actual en formato YYYY-MM-DD, HH:MM:SS
     tstamp = datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
 
+    # ip de origen 
     ip_orig = request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR')
 
+    # guarda los datos recibidos en un archivo de texto
+    # una línea solamente ! --> reemplazar por insert a BBDD
     try:
         f = open("datos.csv", "w")
         linea_datos = temp + ";" + hume + ";" + sens_nro + ";" + tstamp + ";" + ip_orig + "\n"
         f.write(linea_datos)
         f.close()
     except:
-        error_datain += 2
+        pass
+
 
 @route('/')
 def inicio():
+    """
+        muestra home
+    """
     return template('index_base.html')
+
 
 @route('/ver')
 def ver_datos():
+    """
+        procesa el pedido del browser
+        lee los datos del archivo (reemplazar por BBDD!!)
+        y los envía como texto
+    """
     f = open("datos.csv", "r")
     medicion = f.readline().split(";")
     f.close()
@@ -55,14 +75,14 @@ def contacto():
 
 
 @route('/<filepath:path>')
-def server_static(filepath):
-    """ para archivos estáticos (.css, imagenes, etc.) """
+def server_static(filepath):    
+    """ necesario para archivos estáticos (.css, imagenes, etc.) """
     return static_file(filepath, root='.')
 
 
-# run(host='localhost', port=8080, debug=True, reloader=True)
-application = default_app()
+# ejecuta server de desarrollo
+run(host='localhost', port=8080, debug=True, reloader=True)
 
-
-
+# comentar la línea anterior y descomentar la sigiuente para usar en "prod"
+# application = default_app()
 
